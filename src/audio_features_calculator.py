24c08ee4d44cfd1201eb6f2ca8f21de90f655a88
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-06-08 22:53:29 +0200

    replaced yield by list

--- src/audio_features_calculator.py
+++ src/audio_features_calculator.py
@@ -7,26 +7,40 @@ AudioFeatures = namedtuple('AudioFeatures', 'loudness zcr')
 class AudioFeaturesCalculator(object):
     
     def calc(self, audio_data, framesize, framestep):
+        # entspricht f_k
+        frames = self.frames(audio_data.data, framesize, framestep)
+        # entspricht X aller Frames
         # rfft: liefert 1. Hälfte des Frequenzspektrums
-        frame_cmplx_comps = [np.fft.rfft(self.hamming_window(f)) for f in self.frames(audio_data.data, framesize, framestep)]
-        #logging.debug('calced complex frame components {}'.format(frame_cmplx_comps))
-        frame_energy_comps = [np.log10(np.abs(f)) * 10 for f in frame_cmplx_comps]
-        #logging.debug('calced energy frame components {}'.format(frame_energy_comps))
+        frame_cmplx_comps = [np.fft.rfft(self.hamming_window(frame)) for frame in frames]
+        # entspricht E aller Frames
+        frame_energy_comps = [np.log10(np.abs(frame)) * 10 for frame in frame_cmplx_comps]
+        frame_frequencies = [self.frame_freqs(frame, audio_data.samplerate) for frame in frames]
         return AudioFeatures(
-            np.array([self.loudness(f) for f in self.frames(audio_data.data, framesize, framestep)]),
-            np.array([self.zero_crossing_rate(f) for f in self.frames(audio_data.data, framesize, framestep)])),
+            np.array([self.loudness(frame) for frame in frames]),
+            np.array([self.zero_crossing_rate(frame) for frame in frames])),
     
-    # TODO Liste
+
     def frames(self, data, framesize, framestep):
+        frames = []
         for i in range(0, len(data)-(framesize-1), framestep):
             logging.debug('yielding frame {}'.format(range(i,i+framesize)))
-            yield data[i:i+framesize]
+            frames.append(data[i:i+framesize])
+            
+        return frames
+    
+            
+    def frame_freqs(self, frame, samplerate):
+        indices = np.arange(0, len(frame))
+        return indices * samplerate / len(frame)
+    
             
     def loudness(self, frame):
         return np.sum(frame ** 2) / len(frame)
     
+    
     def zero_crossing_rate(self, frame):
         return np.sum(np.abs(np.sign(frame[1:]) - np.sign(frame[:-1]))) / (2 * len(frame))
+    
     
     def hamming_window(self, frame):
         indices = np.arange(0, len(frame))
