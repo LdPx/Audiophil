61f147b737c9128c1d78c9fb499af2a798178dc6
forall <forall>
2017-06-08 10:29:49 +0400

    added ffffffffffffttttttttt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import pprint
 from src.audio_data_reader import AudioDataReader
 import matplotlib.pyplot as plt
+from numpy.fft import irfft
 
 
 def main():
@@ -11,10 +12,27 @@ def main():
     for i, audio_datum in enumerate(audio_data):
         plt.figure(i)
         plt.plot(audio_datum.data)
-        plt.ylabel("amplitude")
+        plt.ylabel("ampl")
         plt.xlabel("time")
         plt.title(audio_datum.name)
-    plt.show()
+    for i, audio_datum in enumerate(audio_data):
+        plt.figure(i + len(audio_data))
+        plt.plot(abs(audio_datum.fdata))
+        plt.ylabel("energy")
+        plt.xlabel("freq")
+        plt.title(audio_datum.name)
+    
+	for i, audio_datum in enumerate(audio_data):
+        res = audio_datum.fdata
+        res[abs(res) < 0.25*10e6] = 0
+        res = irfft(res)
+        plt.figure(i + len(audio_data) * 2)
+        plt.plot(res)
+        plt.ylabel("ampl")
+        plt.xlabel("time")
+        plt.title(audio_datum.name)
+    
+	plt.show()
 
 if __name__ == '__main__':
     main()
--- src/audio_data_reader.py
+++ src/audio_data_reader.py
@@ -1,8 +1,9 @@
 import os
 from collections import namedtuple
 from scipy.io import wavfile
+from numpy.fft import rfft
 
-AudioData = namedtuple('AudioData', 'name data samplerate')
+AudioData = namedtuple('AudioData', 'name data fdata samplerate')
 
 
 class AudioDataReader(object):
@@ -12,6 +13,6 @@ class AudioDataReader(object):
         for root, _dirs, files in os.walk(data_folder):
             for file in sorted(files):
                 res = wavfile.read(os.path.join(root, file))
-                audio_data.append(AudioData(name=file, data=res[1], samplerate=res[0]))
+                audio_data.append(AudioData(name=file, data=res[1], fdata=rfft(res[1]), samplerate=res[0]))
                 
         return audio_data
