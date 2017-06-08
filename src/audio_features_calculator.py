0782d99a6074ad627eef5cb4abf9beb96689db94
DieGabelLinksVomTeller <thomas.schell35@web.de>
2017-06-08 21:13:57 +0200

    refactored frame handling in audiofeaturescalculator

--- src/audio_features_calculator.py
+++ src/audio_features_calculator.py
@@ -8,22 +8,19 @@ class AudioFeaturesCalculator(object):
     
     def calc(self, audio_data, framesize, framestep):
         return AudioFeatures(
-            self.loudness(audio_data, framesize, framestep),
-            self.zero_crossing_rate(audio_data, framesize, framestep))
+            np.array([self.loudness(f) for f in self.frames(audio_data.data, framesize, framestep)]),
+            np.array([self.zero_crossing_rate(f) for f in self.frames(audio_data.data, framesize, framestep)])),
     
     def frames(self, data, framesize, framestep):
         for i in range(0, len(data)-(framesize-1), framestep):
             logging.debug('yielding frame {}'.format(range(i,i+framesize)))
             yield data[i:i+framesize]
             
-    def loudness(self, audio_data, framesize, framestep):
-        return np.array([np.sum(frame ** 2) / len(frame) for frame in self.frames(audio_data.data, framesize, framestep)])
+    def loudness(self, frame):
+        return np.sum(frame ** 2) / len(frame)
     
-    def zero_crossing_rate(self, audio_data, framesize, framestep):
-        frame_zero_crossing_rates = []
-        for frame in self.frames(audio_data.data, framesize, framestep):
-            frame_zero_crossing_rates.append(np.sum(np.abs(np.sign(frame[1:]) - np.sign(frame[:-1]))) / (2 * len(frame)))
-        return np.array(frame_zero_crossing_rates)
+    def zero_crossing_rate(self, frame):
+        return np.sum(np.abs(np.sign(frame[1:]) - np.sign(frame[:-1]))) / (2 * len(frame))
     
     
     
